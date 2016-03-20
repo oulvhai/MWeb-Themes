00ac2a6405243a581a96b859e153fafb809d6a7f
oulvhai <oulvhai@oulvhais-MacBook-Pro.local>
2016-03-20 17:12:46 +0800

    new home page, help page

--- mweb-Bootstrap-blog/asset/js/docs.js
+++ mweb-Bootstrap-blog/asset/js/docs.js
@@ -2,3 +2,34 @@ $(function(){
 	$('.content-responsive img').addClass('img-responsive');
 	$('.content-responsive table').addClass('table').addClass('table-bordered')
 });
+
+function switchImageByName(ele,name,index){
+	var btnG = $(ele).parent();
+	var ctEle = btnG.parent().parent().find('.img_content');
+	btnG.find('.btn').removeClass('disabled');
+	$(ele).addClass('disabled');
+
+	ctEle.find('img').attr('src',name);
+	ctEle.find('p').hide().eq(index).show();
+
+}
+
+
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+}else{
+	preload([
+    'asset/img/cn/1-2.jpg',
+    'asset/img/cn/1-3.jpg',
+    'asset/img/cn/2-2.jpg',
+    'asset/img/cn/3-2.jpg',
+    'asset/img/cn/3-3.jpg',
+    'asset/img/cn/4-2.jpg'
+	]);
+}
